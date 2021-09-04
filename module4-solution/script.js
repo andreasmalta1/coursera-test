afc12954a56c7bbf14d38c03b67514612da0e6b3
andreasmalta1 <38852990+andreasmalta1@users.noreply.github.com>
2021-09-04 17:19:45 +0200

    Add files via upload

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,7 +1,7 @@
 (function (names) {
   for (var i = 0; i < names.length; i++) {
       var firstletter = char = names[i].charAt(0).toLowerCase()
-      if (char=="j"){
+      if (char==="j"){
           helloSpeaker.speak(names[i])
         } else {
           byeSpeaker.speak(names[i])
