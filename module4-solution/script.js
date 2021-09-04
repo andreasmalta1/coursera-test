b763bb4b69ecce4b14c4b2579392f3667661c35f
andreasmalta1 <38852990+andreasmalta1@users.noreply.github.com>
2021-09-04 17:21:56 +0200

    Add files via upload

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,10 +1,11 @@
 (function (names) {
   for (var i = 0; i < names.length; i++) {
       var firstletter = char = names[i].charAt(0).toLowerCase()
-      if (char==="j"){
-          helloSpeaker.speak(names[i])
-        } else {
+      if (char=="j"){
           byeSpeaker.speak(names[i])
+          
+        } else {
+          helloSpeaker.speak(names[i])
         }
       }
 })(["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"]);
